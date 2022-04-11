0ea9cdf7fc6d7be609baec6a349d196aeda80fda
aulth <mohdusman.you@gmail.com>
2022-04-12 02:51:42 +0530

    hindi and urdu language added

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ const goToNext = (current, next)=>{
         document.getElementById('amount').innerHTML = document.getElementById('money').value;
     }
     if(next==='final-screen'){
-        document.getElementById('withdraw').play();
-       setTimeout(()=>{
-           document.getElementById('collect-cash').innerHTML = 'Please collect your cash'
-       }, 13000)
+       document.getElementById('withdraw').play();
+       collectYourCash();
     }
+    checkLang();
 }
 const goToHome = (current)=>{
     document.getElementById('key').play();
@@ -58,4 +57,85 @@ const fastCash = (amount)=>{
     document.getElementById('key').play();
     goToNext('fast-cash-screen', 'pin-screen')
     document.getElementById('amount').innerHTML = amount;
+}
+// scipt for langugage 
+let language = 'english';
+const setLanguage = (lang)=>{
+    language = lang;
+    console.log(language)
+}
+let exitBtn = document.querySelectorAll('.exit');
+let backBtn = document.querySelectorAll('.back');
+let fastCashBtn = document.getElementById('fast-cash-btn');
+let cWBtn = document.getElementById('c-w-btn');
+let ftBtn = document.getElementById('f-t-btn');
+let pinChangeBtn = document.getElementById('pin-change-btn');
+let bEBtn = document.getElementById('b-e-btn');
+let mSBtn = document.getElementById('m-s-btn');
+let amountSelect = document.getElementById('amount-select');
+let enterAmount = document.getElementById('enter-amount');
+let enterPin = document.getElementById('enter-pin');
+let viewAmount = document.getElementById('view-amount');
+let pWait = document.getElementById('p-wait');
+let tProcessing = document.getElementById('t-processing');
+
+const checkLang = ()=>{
+    if(language==='english'){
+        exitBtn.forEach((item)=>{item.innerHTML='Exit'})
+        backBtn.forEach((item)=>{item.innerHTML='Back'})
+        fastCashBtn.innerHTML = 'Fast Cash';
+        cWBtn.innerHTML = 'Cash Withdrawl';
+        ftBtn.innerHTML = 'Fund Transfer';
+        pinChangeBtn.innerHTML = 'Pin Change';
+        bEBtn.innerHTML = 'Balance Enquiry';
+        mSBtn.innerHTML = 'Mini Statement';
+        amountSelect.innerHTML = 'Select the amount for your fast cash transaction';
+        enterAmount.innerHTML = 'Enter the amount';
+        enterPin.innerHTML = 'Enter the pin';
+        viewAmount.innerHTML = 'Amount';
+        pWait.innerHTML = 'Please wait..';
+        tProcessing.innerHTML = 'Transaction is being processed';
+    }else if(language==='hindi'){
+        exitBtn.forEach((item)=>{item.innerHTML='बाहर निकलें'})
+        backBtn.forEach((item)=>{item.innerHTML='पीछे जाएँ'})
+        fastCashBtn.innerHTML = 'तेजी से नकदी';
+        cWBtn.innerHTML = 'नकद निकासी';
+        ftBtn.innerHTML = 'फंड ट्रांसफर';
+        pinChangeBtn.innerHTML = 'पिन बदलें';
+        bEBtn.innerHTML = 'बैलेंस जाँच';
+        mSBtn.innerHTML = 'मिनी स्टेटमेंट';
+        amountSelect.innerHTML = 'अपने तेज़ नकद लेनदेन के लिए राशि का चयन करें';
+        enterAmount.innerHTML = 'राशि दर्ज करें';
+        enterPin.innerHTML = 'पिन दर्ज करें';
+        viewAmount.innerHTML = 'राशि';
+        pWait.innerHTML = 'कृपया प्रतीक्षा कीजिये..';
+        tProcessing.innerHTML = 'लेन-देन संसाधित किया जा रहा है';
+    }else if(language==='urdu'){
+        exitBtn.forEach((item)=>{item.innerHTML='باہر نکلیں'})
+        backBtn.forEach((item)=>{item.innerHTML='پیچھے'})
+        fastCashBtn.innerHTML = 'فاسٹ کیش';
+        cWBtn.innerHTML = 'رقم نکلوانا';
+        ftBtn.innerHTML = 'فنڈ ٹرانسفر';
+        pinChangeBtn.innerHTML = 'پن کی تبدیلی';
+        bEBtn.innerHTML = 'بیلنس انکوائری';
+        mSBtn.innerHTML = 'منی بیان';
+        amountSelect.innerHTML = 'اپنے تیز نقد لین دین کے لیے رقم منتخب کریں۔';
+        enterAmount.innerHTML = 'رقم درج کریں';
+        enterPin.innerHTML = 'پن درج کریں';
+        viewAmount.innerHTML = 'رقم';
+        pWait.innerHTML = '..برائے مہربانی انتظار کریں';
+        tProcessing.innerHTML = 'لین دین پر کارروائی ہو رہی ہے';
+    }
+}
+const collectYourCash = ()=>{
+    setTimeout(()=>{
+        let cCash = document.getElementById('collect-cash');
+        if(language==='english'){
+            cCash.innerHTML = "Please collect your cash";
+        }else if(language==='hindi'){
+            cCash.innerHTML = "कृपया अपना कैश प्राप्त करें";
+        }else if(language==='urdu'){
+            cCash.innerHTML = "براہ کرم اپنا نقد حاصل کریں";
+        }
+    }, 13000)
 }
